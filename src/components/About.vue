1f9d6e58792356636e163af086df7b3c3bb48b95
d070714 <jannik.schroeer@sap.com>
2022-01-04 22:03:03 +0100

    blur

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,11 +7,14 @@ const main = useMainStore()
 
 <template>
   <div>
-    <p class="text-4xl">
+    <p
+      class="text-4xl font-semibold "
+      style="text-shadow: 1px 1px 5px #fff, 1px -1px 31px #ccc;"
+    >
       {{ main.content?.Pitch }}
     </p>
     <img :src="main.content.StackedPhotos?.data?.attributes.url">
-    <p class="text-4xl">
+    <p class="text-2xl">
       {{ main.content.Introduction }}
     </p>
     <img class="p-28" :src="main.content.PhotoBelowIntro?.data?.attributes?.url">
