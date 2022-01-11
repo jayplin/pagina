1f61c28b15251811b8b8dfdc826c9975b0201e50
d070714 <jannik.schroeer@sap.com>
2022-01-11 10:46:58 +0000

    back button imprint

--- src/components/Address.vue
+++ src/components/Address.vue
@@ -5,11 +5,17 @@ const main = useMainStore()
 </script>
 
 <template>
-  <div id="about" class="text-[#ff97ff] p-5">
-    <img src="/logopink.png" class="w-96" alt="">
-
-    <span class="lg:text-3xl text-2xl whitespace-pre-line">
-      {{ main.content?.ImprintAddress }}
-    </span>
+  <div id="about" class="text-[#ff97ff] p-5 flex flex-col justify-between">
+    <div>
+      <img src="/logopink.png" class="w-96" alt="">
+      <span class="lg:text-3xl text-2xl whitespace-pre-line">
+        {{ main.content?.ImprintAddress }}
+      </span>
+    </div>
+    <div>
+      <router-link class="text-xl" to="/">
+        HOME
+      </router-link>
+    </div>
   </div>
 </template>
